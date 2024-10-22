656f7f5ad5127f2c327ce39d6fab5d7a37c4db16
stefanbgh <stefanbgh.po@gmail.com>
2024-10-22 19:46:48 +0200

    type()

--- src/main.py
+++ src/main.py
@@ -1,2 +1,3 @@
 # from parts import part1;
-from parts import part2;
+# from parts import part2;
+from parts import part3;
---
+++ src/parts/part3.py
@@ -0,0 +1,12 @@
+### 3 ###
+# Type
+print("------------------------")
+
+num = 1
+percentage = 45.75
+
+print(type(num))
+print(int(percentage))  # cuts off the decimal part
+print(round(percentage))  # rounds to the nearest whole number
+
+print("------------------------")
