26d3b8bf820b3a6537128540d6b9e2dec635f956
stefanbgh <stefanbgh.po@gmail.com>
2024-10-24 01:31:58 +0200

    keyword arguments

--- src/parts/keyword_arguments.py
+++ src/parts/keyword_arguments.py
@@ -1,7 +1,20 @@
 ### 33 ###
 # keyword arguments
+# *args **kwargs
 
-def ka(x, y, z):
-    print(f"First: {x}\nSecond: {y}\nLast: {z}")
+def ka(*args, **kwargs):
+    print("--------------------------")
 
-ka(z=3, x=1, y=2)
+    for arg in args:
+        print(arg)
+
+    # print("------------------------")
+    # for kwarg in kwargs.values():
+    #     print(f"{kwarg}", end=" ")
+    # print()
+
+    print("--------------------------")
+    print(f"{kwargs.get("first_name")} {kwargs.get("last_name")} {kwargs.get("dash")} {kwargs.get("version")}")
+    print("--------------------------")
+
+ka(3, 2, 1, "Wazzzzupppp!", first_name="Sponge", last_name="Bob", dash="-", version="Square Pants")
