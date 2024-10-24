b87c218f72b03f2520715c552ac61ba64c4e7acc
stefanbgh <stefanbgh.po@gmail.com>
2024-10-24 13:10:07 +0200

    magic methods

--- src/parts/class_variables.py
+++ src/parts/class_variables.py
@@ -1,6 +1,14 @@
 ### 47 ###
 # class
 
+# magic methods 
+# __eq__       -> ==
+# __lt__       -> <
+# __gt__       -> >
+# __add__      -> +
+# __contains__ -> in
+# __getitem__  -> if
+
 print("------------------------")
 class User:
     year = 2024
@@ -11,6 +19,10 @@ class User:
         self.email = email
         self.password = password
         User.year += 1
+
+    def __str__(self):
+        return f"First Name: {self.first_name}\nLast Name: {self.last_name}\nEmail: {self.email}\nPassword: {self.password}"
+
 
     def get_info(self):
         return f"First Name: {self.first_name}\nLast Name: {self.last_name}\nEmail: {self.email}\nPassword: {self.password}"
@@ -25,10 +37,13 @@ class User:
 
 user1 = User("Stefan", "Blagojevic", "stefan@gmail.com", "qwerty")
 
-res = user1.get_info()
+# res = user1.get_info()
 sm = User.fn()
 
-print(res)
+# __str__
+get_info = user1 # don't need to call the method .get_info()
+
+print(get_info)
 print("------------------------")
 print(sm)
 print(f"Year: {User.get_year()}")
